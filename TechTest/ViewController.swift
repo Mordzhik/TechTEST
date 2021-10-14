4aedc575e80be892abba83ada68168e6b0463db4
Marzhan <nurl-imarzhan@mail.ru>
2021-10-14 10:43:01 +0600

    Initial Commit

---
+++ TechTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechTest
+//
+//  Created by tse on 10/14/21.
+//  Copyright © 2021 marzhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
